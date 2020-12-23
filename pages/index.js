041410cb83b65f831cd561ec9336be7e2ab0c93c
Visshu Kashyap <kashyapvisshu47@gmailc.om>
2020-12-23 16:04:15 +0530

    nextjs project

---
+++ pages/index.js
@@ -0,0 +1,40 @@
+import Link from 'next/link'
+import utilStyles from '../styles/utils.module.css';
+import Layout from '../components/layout';
+import { getSortedPostsData } from '../lib/posts';
+import Date from '../components/date';
+
+export async function getStaticProps(){
+  const allPostData = getSortedPostsData();
+  return {
+     props: {
+      allPostData
+     }
+  }
+}
+
+export default function Home({ allPostData }) {
+  return (
+    <Layout home>
+      {/* Keep the existing code here */}
+
+      {/* Add this <section> tag below the existing <section> tag */}
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+            <Link href={`/posts/${id}`}>
+              <a>{title}</a>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
+          </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  )
+}
